49d738fbf3c36ffea2a27d6e13789775d48e0c17
Daniel Nicolai <dalanicolai@gmail.com>
2021-07-29 09:47:14 +0200

    Update csv2meta.py to python3

--- examples/csv2meta.py
+++ examples/csv2meta.py
@@ -14,25 +14,25 @@ args = parser.parse_args()
 delim = args.d               # requested CSV delimiter character
 assert args.csv, "missing CSV filename"
 assert args.pdf, "missing PDF filename"
-print "delimiter", args.d
-print "xml delete", args.x
-print "csv file", args.csv
-print "pdf file", args.pdf
-print "----------------------------------------"
+print("delimiter", args.d)
+print("xml delete", args.x)
+print("csv file", args.csv)
+print("pdf file", args.pdf)
+print("----------------------------------------")
 doc = fitz.open(args.pdf)
 oldmeta = doc.metadata
-print "old metadata:"
+print("old metadata:")
 for k,v in oldmeta.items():
-    print k, ":",v
+    print(k, ":",v)
 
 with open(args.csv) as tocfile:
     tocreader = csv.reader(tocfile, delimiter = delim)
     for row in tocreader:
         assert len(row) == 2, "each row must contain 2 entries"
         oldmeta[row[0]] = row[1]
-print "----------------------------------------"
-print "\nnew metadata:"
+print("----------------------------------------")
+print("\nnew metadata:")
 for k,v in oldmeta.items():
-    print k, ":",v
+    print(k, ":",v)
 doc.set_metadata(oldmeta)
 doc.saveIncr()
